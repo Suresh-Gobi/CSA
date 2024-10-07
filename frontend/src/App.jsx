cf8137d456cb7bd5116e004133ab83cbbca6a7a9
Suresh_Gobi <sureshgobi34@gmail.com>
2024-10-07 17:59:11 +0530

    signup setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 // import store from "./store";
 
+import Signup from "./pages/auth/signup/signup";
+
 function App() {
   return (
     // <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     // </Provider>
