433b548f7449ea2ac0a07fa89dc36c0b5016b7e4
Suresh_Gobi <90418151+Suresh-Gobi@users.noreply.github.com>
2024-10-30 20:41:41 +0530

    Update User Details

--- backend/Controllers/user.controller.js
+++ backend/Controllers/user.controller.js
@@ -1,4 +1,4 @@
-const db = require('../Models/index');
+const db = require("../Models/index");
 const { User } = db;
 const jwt = require("jsonwebtoken");
 
@@ -14,7 +14,7 @@ exports.getAllUsers = async (req, res) => {
     console.error("Error fetching users:", error);
     res.status(500).json({
       success: false,
-      message: 'Server error while fetching users',
+      message: "Server error while fetching users",
     });
   }
 };
@@ -51,7 +51,10 @@ exports.getLoggedUserDetails = async (req, res) => {
     });
   } catch (error) {
     console.error("Error fetching user details:", error);
-    if (error.name === "JsonWebTokenError" || error.name === "TokenExpiredError") {
+    if (
+      error.name === "JsonWebTokenError" ||
+      error.name === "TokenExpiredError"
+    ) {
       return res.status(401).json({
         success: false,
         message: "Invalid or expired token",
@@ -61,5 +64,67 @@ exports.getLoggedUserDetails = async (req, res) => {
       success: false,
       message: "Server error while fetching user details",
     });
+  }
+};
+
+// Update User Details
+exports.updateLoggedUserDetails = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "No token provided or incorrect format",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+    const {
+      username,
+      email,
+      first_name,
+      last_name,
+      phone_number,
+      profile_picture,
+      date_of_birth,
+      address,
+      password,
+    } = req.body;
+
+    // Find the user by ID
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (username) user.username = username;
+    if (email) user.email = email;
+    if (first_name) user.first_name = first_name;
+    if (last_name) user.last_name = last_name;
+    if (phone_number) user.phone_number = phone_number;
+    if (profile_picture) user.profile_picture = profile_picture;
+    if (date_of_birth) user.date_of_birth = date_of_birth;
+    if (address) user.address = address;
+    if (password) user.password = password;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "User details updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error"
+    })
   }
 };
